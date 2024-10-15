b8140dec16924916524595b8de3f0c7c8e212e0f
mimil_grp <mimil.group.49@gmail.com>
2024-10-15 09:29:54 +0200

    update index php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device, initial-scale=1.0">
     <link rel="icon" type="image/x-icon" href="fav.ico">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Geologica">
@@ -13,32 +13,31 @@
 </head>
 
 <body>
-    <nav class="navbar navbar-dark sticky-top">
+    <nav class="navbar navbar-dark sticky-top navbar-top">
         <h2>Azure AI Object Recognition</h2>
     </nav>
     <div class="container">
         <div class="row">
-            <div class="col-lg-5">
-                <div class="container-fluid zone">
+            <div class="col col-lg-5">
+                <div class="container-fluid div-zone">
                     <h3>Import a picture</h3>
                     <form action="" method="post" enctype="multipart/form-data">
-                        <label class="input input-button" for="picture">Choisir une photo</label>
-                        <input class="input-hidden" type="file" id="picture" name="picture" accept="image/png, image/gif, image/jpeg"><br>
-                        <img id="apercu" src=""><br>
-                        <input class="input input-button" type="submit" id="envoyer" value="Envoyer" disabled>
+                        <input class="input-hidden" type="file" id="picture" name="input-img" accept="image/png, image/jpeg"><br>
+                        <img class="img-overview" id="img-overview" src="icon.png"><br>
+                        <input class="input input-button" type="submit" id="envoyer" value="Submit">
                     </form>
                 </div>
             </div>
-            <div class="col-lg-7">
-                <div class="container-fluid zone">
-                    <h3>Azure AI Object Recognition</h3>
-                    <pre id=""></pre>
+            <div class="col col-lg-7">
+                <div class="container-fluid div-zone">
+                    <h3>AI Output</h3>
+                    <pre class="ai-output" id="ai-output">output</pre>
                 </div>
             </div>
         </div>
     </div>
-    <nav class="navbar navbar-dark fixed-bottom">
-        <p class="credits">Powered by OUVRARD Emilien, MERYET Benjamin and ASTRUC Mathieu</p>
+    <nav class="navbar navbar-dark justify-content-center fixed-bottom navbar-bottom">
+        <p class="copyright">&copy; 2024 OUVRARD Emilien, MERYET Benjamin and ASTRUC Mathieu</p>
     </nav>
 </body>
 
